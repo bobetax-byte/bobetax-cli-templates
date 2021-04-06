9d235951f9abada86b2e2a032a6de46815e61508
zhoubo <1522306890@qq.com>
2021-04-06 16:26:33 +0800

    add question.js

---
+++ question.js
@@ -0,0 +1,20 @@
+/**
+ * @description  用户输入信息
+ */
+ module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'This is a private registry?'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: "what's your author name"
+  },
+  {
+    type: 'description',
+    name: 'description',
+    message: "what's your description"
+  }
+]
