8bd74eba317ee2869de5c9fc4311c71658297a3b
zhoubo <1522306890@qq.com>
2021-04-07 17:31:35 +0800

    feat: 🎸 main.js
    入口处添加兼容代码

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "regenerator-runtime/runtime";
+import "core-js/stable";
+
 Vue.config.productionTip = false;
 
 new Vue({
