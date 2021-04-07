265c458330ed699a3c962726052d0cac313ccdd8
zhoubo <1522306890@qq.com>
2021-04-07 13:51:36 +0800

    style: 💄 change eslint styles
    修改了样式显示

--- question.js
+++ question.js
@@ -1,26 +1,26 @@
 /**
  * @description  用户输入信息
  */
- module.exports = [
+module.exports = [
   {
-    type: 'confirm',
-    name: 'private',
-    message: 'This is a private registry?'
+    type: "confirm",
+    name: "private",
+    message: "This is a private registry?",
   },
   {
-    type: 'input',
-    name: 'author',
-    message: "what's your author name"
+    type: "input",
+    name: "author",
+    message: "what's your author name",
   },
   {
-    type: 'input',
-    name: 'description',
-    message: "what's your description"
+    type: "input",
+    name: "description",
+    message: "what's your description",
   },
   {
-    type: 'list',
-    choices: ['MIT', 'apache-2.0'],
-    name: 'license',
-    message: "what's your description"
-  }
-]
+    type: "list",
+    choices: ["MIT", "apache-2.0"],
+    name: "license",
+    message: "what's your description",
+  },
+];
