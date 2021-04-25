88f8199e76345af453b6a9c379b1e067ddd40dc5
zhoubo <1522306890@qq.com>
2021-04-25 09:35:36 +0800

    feat: 🎸 question.js
    添加了新的文件

---
+++ question.js
@@ -0,0 +1,37 @@
+/**
+ * @description  用户输入信息
+ */
+ module.exports = [
+  {
+    type: "confirm",
+    name: "private",
+    message: "This is a private registry?",
+  },
+  {
+    type: "input",
+    name: "version",
+    message: "what's your project's version?",
+    default: "1.0.0",
+  },
+  {
+    type: "input",
+    name: "author",
+    message: "what's your author name",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "what's your description",
+  },
+  {
+    type: "list",
+    choices: ["MIT", "apache-2.0"],
+    name: "license",
+    message: "what's your description",
+  },
+  {
+    type: "confirm",
+    name: "compatibility",
+    message: "Compatible with IE?",
+  },
+];
